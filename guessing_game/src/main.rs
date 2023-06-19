7200e1d24e023be44cb61e89cabc986f73b03dc9
PAUL OFFEI <poffei@st.ug.edu.gh>
2023-06-19 12:21:26 +0000

    refactor and cleand the code

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,13 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
+
 
 fn compare(guess: u32, rand: u32) {
     match guess.cmp(&rand) {
-        Ordering::Less => println!("Too Low::guess = {guess} < rand = {rand}"),
-        Ordering::Greater => println!("Too High::guess = {guess} > rand = {rand}"),
-        Ordering::Equal => println!("Winner::guess = {guess} == rand = {rand}"),
+        Ordering::Less => println!("Too Low::guess is {guess}"),
+        Ordering::Greater => println!("Too High::guess is {guess}"),
+        Ordering::Equal => println!("Winner::guess is {guess} and rand is {rand}"),
     }
 }
 
@@ -17,21 +18,30 @@ fn os_random() -> u32 {
 
 fn guess() {
     let rand: u32 = os_random();
-    println!("Guess your favourite number!!!::!!!)");
-    let mut guess: String = String::new();
-    // io::stdin().read_line(&mut guess).expect("Failed to read the message");
-    io::stdin().read_line(&mut guess).unwrap();
+    loop {
+        println!("Guess your favourite number!!!::!!!)");
+        let mut guess: String = String::new();
+        
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read the input");
     
-    let guess: u32 = guess.trim().parse().expect("I'm expecting a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    compare(guess, rand);
+        compare(guess, rand);
+        
+        if guess == rand {
+            break;
+        }
+    }  
 }
 
 
 
 fn main() {
     println!("Welcome to Guess the Number Game::)");
-    loop {
-        guess();
-    }
+    guess();
 }
